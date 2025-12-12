cbf399e43b6b068ca7ee40afb32daeed552f96db
Ahra-web <ahraon0106@gmail.com>
2025-12-13 03:20:26 +0900

    commit

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseServer = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!, // ✅ 서버 전용 키
   { auth: { persistSession: false } }
 );
